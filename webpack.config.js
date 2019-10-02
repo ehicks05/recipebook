3af32a6cff9574c14a9ee246506aefd08ca6a437
Eric Hicks <ehicks05@yahoo.com>
2019-10-01 21:04:11 -0400

    Add loader for css.

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,17 @@ module.exports = {
                 use: {
                     loader: "babel-loader"
                 }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader'
+                    }
+                ]
             }
         ]
     }
