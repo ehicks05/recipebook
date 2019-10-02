995fd73477eab2856bfda808aa75d3d6513798ad
Eric Hicks <ehicks05@yahoo.com>
2019-10-01 21:04:58 -0400

    Make sure styles are working.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
+import 'bulma/css/bulma.css'
 
 ReactDOM.render(
-    <h1>Hello, World</h1>,
+    <section class="section">
+        <div class="container">
+            <h1 class="title">
+                Hello World
+            </h1>
+            <p class="subtitle">
+                My first website with <strong>Bulma</strong>!
+            </p>
+        </div>
+    </section>
+    ,
     document.getElementById('root')
-)
+);
