c2892a5b2bd6bc380a54adaef0c1c6d1aa00dc48
shicks <shicks255@yahoo.com>
2019-10-01 22:09:50 -0500

    adding more react skeleton, and recipes.json file

--- src/index.js
+++ src/index.js
@@ -1,18 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
+import App from './App'
 import 'bulma/css/bulma.css'
 
-ReactDOM.render(
-    <section class="section">
-        <div class="container">
-            <h1 class="title">
-                Hello World
-            </h1>
-            <p class="subtitle">
-                My first website with <strong>Bulma</strong>!
-            </p>
-        </div>
-    </section>
-    ,
-    document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
