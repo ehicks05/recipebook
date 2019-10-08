f47c278618d088711da8ba55eace9227f0b25323
Eric Hicks <ehicks05@yahoo.com>
2019-10-07 23:26:56 -0400

    Add initial draft of a sidebar.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import recipeData from './recipes.json';
 import Recipe from './components/Recipe';
 import RecipePicker from './components/RecipePicker';
 import Navbar from "./components/Navbar";
+import Sidebar from "react-sidebar";
+
+const mql = window.matchMedia(`(min-width: ` + (768 + 286) + `px)`);
 
 export default class App extends React.Component {
 
@@ -10,10 +13,14 @@ export default class App extends React.Component {
         super(props);
 
         this.handleClickRecipe = this.handleClickRecipe.bind(this);
+        this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
+        this.mediaQueryChanged = this.mediaQueryChanged.bind(this);
 
         this.state = {
             recipes: recipeData,
             selectedRecipeId: 1,
+            sidebarDocked: mql.matches,
+            sidebarOpen: false
         }
     }
 
@@ -21,35 +28,60 @@ export default class App extends React.Component {
         this.setState({ selectedRecipeId: id });
     }
 
+    componentDidMount() {
+        mql.addEventListener("change", this.mediaQueryChanged);
+    }
+
+    componentWillUnmount() {
+        mql.removeEventListener("change", this.mediaQueryChanged);
+    }
+
+    onSetSidebarOpen(open) {
+        this.setState({ sidebarOpen: open });
+    }
+
+    mediaQueryChanged() {
+        this.setState({ sidebarDocked: mql.matches, sidebarOpen: false });
+    }
+
     render() {
         const selectedRecipe = recipeData.find(item => item.id === this.state.selectedRecipeId);
 
         return (
             <>
-                <Navbar recipe={selectedRecipe} />
-                <section className={"hero is-info"}>
-                    <div className={"hero-body"}>
-                        <div className={"container"}>
-                            <h1 className='title'>{selectedRecipe.name}</h1>
-                            <h3 className='subtitle'>Cooking Time: {selectedRecipe["Cooking Time"]} - Difficulty: {selectedRecipe.difficulty}</h3>
-                        </div>
-                    </div>
-                </section>
+                <Sidebar
+                    sidebar={
+                        <RecipePicker
+                            onClickRecipe={this.handleClickRecipe}
+                            currentlySelected={this.state.selectedRecipeId}
+                            recipes={recipeData}/>
+                    }
+                    open={this.state.sidebarOpen}
+                    docked={this.state.sidebarDocked}
+                    onSetOpen={this.onSetSidebarOpen}
 
-                <section className={"section"}>
-                    <div className={'container'}>
-                        <div className={'columns'}>
-                            <div className={'column is-one-quarter'}>
-                                    <RecipePicker
-                                        onClickRecipe={this.handleClickRecipe}
-                                        currentlySelected={this.state.selectedRecipeId}
-                                        recipes={recipeData} />
+                    styles={{ sidebar: { background: "white" } }}
+                    touchHandleWidth={40}
+                >
+
+                    <Navbar recipe={selectedRecipe} />
+                    <section className={"hero is-info"}>
+                        <div className={"hero-body"}>
+                            <div className={"container"}>
+                                <h1 className='title'>{selectedRecipe.name}</h1>
+                                <h3 className='subtitle'>Cooking Time: {selectedRecipe["Cooking Time"]} - Difficulty: {selectedRecipe.difficulty}</h3>
                             </div>
-
-                            <Recipe recipe={selectedRecipe} />
                         </div>
-                    </div>
-                </section>
+                    </section>
+
+                    <section className={"section"}>
+                        <div className={'container'}>
+                            <div className={'columns'}>
+                                <Recipe recipe={selectedRecipe} />
+                            </div>
+                        </div>
+                    </section>
+                </Sidebar>
             </>
         );
     }
