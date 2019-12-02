b46fc1fd7fb2af565ef56fc784a233b1ecec32fd
Eric Hicks <ehicks05@yahoo.com>
2019-12-01 22:20:09 -0500

    Add a simple timer to any steps of a direction that contain the words 'hour' or 'minute' preceded by a parseable number.

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 
 function Recipe(props) {
     const recipe = props.recipe;
@@ -9,7 +9,7 @@ function Recipe(props) {
     const directions = (
         <div className={'content'}>
             <ol>
-                {recipe.directions.map((direction) => <li key={direction}>{direction}</li>)}
+                {recipe.directions.map((direction) => <Direction key={direction} direction={direction} />)}
             </ol>
         </div>
     );
@@ -38,6 +38,106 @@ function Recipe(props) {
             </div>
         </>
     );
+}
+
+function Direction(props) {
+    const direction = props.direction;
+
+    const words = direction.split(' ');
+    const timeIndex = words.findIndex(word => word.indexOf('minute') > -1 || word.indexOf('hour') > -1);
+    let timeAmount = 0;
+    if (timeIndex > -1)
+    {
+        timeAmount = parseInt(words[timeIndex - 1], 10);
+        if (isNaN(timeAmount))
+            timeAmount = 0;
+
+        let isHours = false;
+        if (words[timeIndex].indexOf('hour') > -1)
+            isHours = true;
+
+        if (isHours)
+            timeAmount *= 60;
+    }
+
+    const timer = timeAmount > 0 ? <Timer minutes={timeAmount}/> : null;
+
+    return (
+        <li key={direction}>{direction} {timer}</li>
+    );
+}
+
+class Timer extends Component {
+    constructor(props)
+    {
+        super(props);
+        this.startTimer = this.startTimer.bind(this);
+        this.stopTimer = this.stopTimer.bind(this);
+
+        this.state = {
+            minutes: props.minutes,
+            seconds: 0,
+            paused: true
+        };
+    }
+
+    startTimer() {
+        this.myInterval = setInterval(() => {
+            const { seconds, minutes } = this.state;
+
+            if (seconds > 0) {
+                this.setState(({ seconds }) => ({
+                    seconds: seconds - 1
+                }))
+            }
+            if (seconds === 0) {
+                if (minutes === 0) {
+                    clearInterval(this.myInterval)
+                } else {
+                    this.setState(({ minutes }) => ({
+                        minutes: minutes - 1,
+                        seconds: 59
+                    }))
+                }
+            }
+        }, 1000);
+
+        this.setState({paused: false})
+    }
+
+    stopTimer() {
+        clearInterval(this.myInterval);
+        this.setState({paused: true});
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.myInterval)
+    }
+
+    render() {
+        const { minutes, seconds } = this.state;
+
+        const button = this.state.paused ?
+            <button className={'button is-small'} onClick={this.startTimer}>Start</button>
+            :
+            <button className={'button is-small'} onClick={this.stopTimer}>Pause</button>
+        ;
+
+        return (
+            <div>
+                <div className="field has-addons">
+                    <p className="control">
+                        <a className="button is-static is-small">
+                            {minutes}:{seconds < 10 ? `0${seconds}` : seconds}
+                        </a>
+                    </p>
+                    <p className="control is-expanded">
+                        {button}
+                    </p>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default Recipe;
