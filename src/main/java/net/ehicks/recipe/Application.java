cc1fe649fc877e23db692060171f45be75896776
Eric Hicks <ehicks05@yahoo.com>
2020-05-20 20:50:10 -0400

    Trigger startup code on ContextRefreshedEvent instead of using a commandLineRunner.

--- src/main/java/net/ehicks/recipe/Application.java
+++ src/main/java/net/ehicks/recipe/Application.java
@@ -1,21 +1,12 @@
 package net.ehicks.recipe;
 
-import org.apache.catalina.Context;
-import org.apache.tomcat.util.scan.StandardJarScanner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.security.core.session.SessionRegistry;
-import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.web.filter.ShallowEtagHeaderFilter;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
@@ -28,33 +19,9 @@ public class Application
 {
     private static final Logger log = LoggerFactory.getLogger(Application.class);
 
-    private Startup startup;
-
-    @Autowired
-    public Application(Startup startup)
-    {
-        this.startup = startup;
-    }
-
     public static void main(String[] args)
     {
         SpringApplication.run(Application.class, args);
-    }
-
-    @Bean
-    public CommandLineRunner commandLineRunner(ApplicationContext ctx)
-    {
-        return args -> {
-            try
-            {
-                startup.start();
-            }
-            catch (Exception e)
-            {
-                System.out.println(e.getMessage());
-                e.printStackTrace();
-            }
-        };
     }
 
     @Bean(name = "multipartResolver")
@@ -69,19 +36,6 @@ public class Application
         return new ShallowEtagHeaderFilter();
     }
 
-//    @Bean
-//    public TomcatServletWebServerFactory tomcatFactory()
-//    {
-//        return new TomcatServletWebServerFactory()
-//        {
-//            @Override
-//            protected void postProcessContext(Context context)
-//            {
-//                ((StandardJarScanner) context.getJarScanner()).setScanManifest(false);
-//            }
-//        };
-//    }
-//
 //    @Bean
 //    SessionRegistry sessionRegistry() {
 //        return new SessionRegistryImpl();
--- src/main/java/net/ehicks/recipe/StartupListener.java
+++ src/main/java/net/ehicks/recipe/StartupListener.java
@@ -3,29 +3,29 @@ package net.ehicks.recipe;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Startup
+public class StartupListener implements ApplicationListener<ContextRefreshedEvent>
 {
-    private static final Logger log = LoggerFactory.getLogger(Startup.class);
+    private static final Logger log = LoggerFactory.getLogger(StartupListener.class);
 
     @Value("${recipeBook.seedDbIfEmpty:false}")
     public String seedDbIfEmpty;
 
-    private Seeder seeder;
+    private final Seeder seeder;
 
-    public Startup(Seeder seeder)
+    public StartupListener(Seeder seeder)
     {
         this.seeder = seeder;
     }
 
-    public void start()
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event)
     {
         log.info("Recipe Book starting...");
-
         seeder.seed(seedDbIfEmpty.toLowerCase().equals("true"));
-
-
     }
 }
