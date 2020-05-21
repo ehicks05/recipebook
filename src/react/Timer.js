8e8422c641bc18af51fe4b422f0614cb64827a27
Eric Hicks <ehicks05@yahoo.com>
2020-05-21 00:33:50 -0400

    Refactor Timer.js from class component to functional component with hooks.

--- src/react/Timer.js
+++ src/react/Timer.js
@@ -1,76 +1,63 @@
-import React, {Component} from "react";
+import React, {useEffect, useRef, useState} from "react";
 
-class Timer extends Component {
-    constructor(props)
-    {
-        super(props);
-        this.startTimer = this.startTimer.bind(this);
-        this.stopTimer = this.stopTimer.bind(this);
+function Timer(props) {
+    const [seconds, setSeconds] = useState(props.minutes * 60);
+    const [paused, setPaused] = useState(true);
+    const [myInterval, setMyInterval] = useState(0);
 
-        this.state = {
-            minutes: props.minutes,
-            seconds: 0,
-            paused: true
-        };
+    const secondsRef = useRef(seconds);
+
+    useEffect(
+        () => { secondsRef.current = seconds },
+        [seconds]
+    );
+
+    useEffect(() => {
+        if (paused) {
+            clearInterval(myInterval);
+        } else {
+            startTimer();
+        }
+    }, [paused]);
+
+    useEffect(() => {
+        return () => {
+            clearInterval(myInterval);
+        }
+    }, []);
+
+    function decrement() {
+        setSeconds(secondsRef.current - 1);
     }
 
-    startTimer() {
-        this.myInterval = setInterval(() => {
-            const { seconds, minutes } = this.state;
-
-            if (seconds > 0) {
-                this.setState(({ seconds }) => ({
-                    seconds: seconds - 1
-                }))
-            }
-            if (seconds === 0) {
-                if (minutes === 0) {
-                    clearInterval(this.myInterval)
-                } else {
-                    this.setState(({ minutes }) => ({
-                        minutes: minutes - 1,
-                        seconds: 59
-                    }))
-                }
-            }
-        }, 1000);
-
-        this.setState({paused: false})
+    function startTimer() {
+        setMyInterval(setInterval(decrement, 1000));
     }
 
-    stopTimer() {
-        clearInterval(this.myInterval);
-        this.setState({paused: true});
+    function togglePause() {
+        setPaused(!paused);
     }
 
-    componentWillUnmount() {
-        clearInterval(this.myInterval)
+    function displayTime() {
+        const m = Math.floor(seconds / 60);
+        const s = seconds % 60;
+        return m + ':' + (s < 10 ? `0${s}` : s);
     }
 
-    render() {
-        const { minutes, seconds } = this.state;
-
-        const button = this.state.paused ?
-            <button className={'button is-small'} onClick={this.startTimer}>Start</button>
-            :
-            <button className={'button is-small'} onClick={this.stopTimer}>Pause</button>
-        ;
-
-        return (
-            <div>
-                <div className="field has-addons">
-                    <p className="control">
-                        <a className="button is-static is-small">
-                            {minutes}:{seconds < 10 ? `0${seconds}` : seconds}
-                        </a>
-                    </p>
-                    <p className="control is-expanded">
-                        {button}
-                    </p>
-                </div>
+    return (
+        <div>
+            <div className="field has-addons">
+                <p className="control">
+                    <a className="button is-static is-small">
+                        {displayTime()}
+                    </a>
+                </p>
+                <p className="control is-expanded">
+                    <button className={'button is-small'} onClick={togglePause}>{paused ? 'Start' : 'Pause'}</button>
+                </p>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default Timer;
