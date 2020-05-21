97aa6fefb5bf90fa6890710c6188479f8d6617b9
Eric Hicks <ehicks05@yahoo.com>
2020-05-21 12:10:42 -0400

    Convert Navbar.js from class-based to functional component.

--- src/react/Navbar.js
+++ src/react/Navbar.js
@@ -1,36 +1,35 @@
 import React from "react";
 
-export default class Navbar extends React.Component {
+function Navbar(props) {
 
-    render() {
+    const dockSidebarButton = !props.sidebarDocked ?
+        <button className={'button bigger-burger has-text-grey is-hidden-touch'} style={{border: 'none', position: 'absolute'}} onClick={() => props.onSetSidebarDocked(true)}>
+            <i className="icon-bars" aria-hidden="true"> </i>
+        </button>
+        : null;
 
-        const dockSidebarButton = !this.props.sidebarDocked ?
-            <button className={'button bigger-burger has-text-grey is-hidden-touch'} style={{border: 'none', position: 'absolute'}} onClick={() => this.props.onSetSidebarDocked(true)}>
-                <i className="icon-bars" aria-hidden="true"> </i>
-            </button>
-            : null;
+    const isSetOpen = !props.sidebarOpen;
+    const icon = props.sidebarOpen ? 'icon-arrow-left' : 'icon-bars';
 
-        const isSetOpen = !this.props.sidebarOpen;
-        const icon = this.props.sidebarOpen ? 'icon-arrow-left' : 'icon-bars';
+    const openSidebarButton =
+        <button className={'button bigger-burger has-text-grey is-hidden-desktop'} style={{border: 'none'}} onClick={() => props.onSetSidebarOpen(isSetOpen)}>
+            <i className={icon} aria-hidden="true"> </i>
+        </button>;
 
-        const openSidebarButton =
-            <button className={'button bigger-burger has-text-grey is-hidden-desktop'} style={{border: 'none'}} onClick={() => this.props.onSetSidebarOpen(isSetOpen)}>
-                <i className={icon} aria-hidden="true"> </i>
-            </button>;
+    return (
+        <nav className="navbar" role="navigation" aria-label="main navigation">
+            {dockSidebarButton}
 
-        return (
-            <nav className="navbar" role="navigation" aria-label="main navigation">
-                {dockSidebarButton}
-
-                <div className={'container'}>
-                    <div className="navbar-brand">
-                        {openSidebarButton}
-                        <a className="navbar-item" href="#">
-                            <span className={'title'}><span role="img" aria-label="plate of food">🍛</span> Recipe Book</span>
-                        </a>
-                    </div>
+            <div className={'container'}>
+                <div className="navbar-brand">
+                    {openSidebarButton}
+                    <a className="navbar-item" href="#">
+                        <span className={'title'}><span role="img" aria-label="plate of food">🍛</span> Recipe Book</span>
+                    </a>
                 </div>
-            </nav>
-        );
-    }
+            </div>
+        </nav>
+    );
 }
+
+export default Navbar;
