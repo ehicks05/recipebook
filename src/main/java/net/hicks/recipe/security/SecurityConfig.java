70ad848ef54a4c1ac3d35b7d715ab25eadf753bf
Eric Hicks <ehicks05@yahoo.com>
2020-05-31 11:20:52 -0400

    Make the authentication entry point return a 401 instead of redirecting to '/login'.

--- src/main/java/net/hicks/recipe/security/SecurityConfig.java
+++ src/main/java/net/hicks/recipe/security/SecurityConfig.java
@@ -2,12 +2,14 @@ package net.hicks.recipe.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -52,6 +54,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
                     .antMatchers("/**").hasRole("USER")
                     .and()
                     .formLogin()
+                    .and()
+                    .exceptionHandling().authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
                     .and()
                     .logout()
                     .and().csrf().disable();
