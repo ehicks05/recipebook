2248d3023f18bcbb2e0364fb54adfe4b2ead6f2c
Eric Hicks <ehicks05@yahoo.com>
2020-05-31 17:05:45 -0400

    authFetch now detects 401 and 403 responses and logs them appropriately.

--- src/react/authFetch.js
+++ src/react/authFetch.js
@@ -1,9 +1,26 @@
-async function authFetch(url, options) {
-    const responseJson = await fetch(url, {...options});
-    // if (responseJson.url.endsWith('/login'))
-    //     throw new Error('Not logged in');
+function authFetch(url, options) {
 
-    return await responseJson.json();
+    // todo: make use of these in production
+    // const csrfHeader = document.head.querySelector("[name~=_csrf_header][content]");
+    // const csrfToken = document.head.querySelector("[name~=_csrf][content]");
+
+    function buildErrorMessage(response)
+    {
+        let message = '';
+        if (response.status === 401) message = 'authenticated';
+        if (response.status === 403) message = 'authorized';
+        return "Must be " + message + " to access '" + url + "'";
+    }
+
+    return fetch(url, {...options})
+        .then(response => {
+            if (!response.ok)
+                throw new Error(buildErrorMessage(response));
+            return response.json()  // todo: if we ever want something other than json back, just return the response?
+        })
+        .catch((error) => {
+            console.log(error);
+        });
 }
 
 export default authFetch;
