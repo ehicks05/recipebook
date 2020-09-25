b64f381854d3bb36824c49c1fae8731c216841d0
shicks <shicks255@yahoo.com>
2020-09-24 21:56:49 -0500

    running prettier on all the files

--- src/react/components/Home.tsx
+++ src/react/components/Home.tsx
@@ -1,23 +1,25 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import React from "react";
 
 export function Home() {
-    return (
-        <>
-            <section className={"hero is-info"}>
-                <div className={"hero-body"}>
-                    <div className={"container"}>
-                        <h1 className='title'>Welcome to the Home Page!</h1>
-                    </div>
-                </div>
-            </section>
-            <section className='section'>
-                <div className={"container"}>
-                    <Link to='/create-recipe'>
-                        <button className='button is-success is-light'>Create Recipe!</button>
-                    </Link>
-                </div>
-            </section>
-        </>
-    );
+  return (
+    <>
+      <section className={"hero is-info"}>
+        <div className={"hero-body"}>
+          <div className={"container"}>
+            <h1 className="title">Welcome to the Home Page!</h1>
+          </div>
+        </div>
+      </section>
+      <section className="section">
+        <div className={"container"}>
+          <Link to="/create-recipe">
+            <button className="button is-success is-light">
+              Create Recipe!
+            </button>
+          </Link>
+        </div>
+      </section>
+    </>
+  );
 }
