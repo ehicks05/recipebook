b35792c9a028b10991b629d103fd9008f2cb7bcb
Eric Hicks <ehicks05@yahoo.com>
2020-10-08 21:54:29 -0400

    Fix bulma-prefers-dark dependency and
    remove generated css file

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import App from './App'
-import './App.css'
+import './App.scss'
 import {BrowserRouter} from "react-router-dom";
 
 ReactDOM.render(
