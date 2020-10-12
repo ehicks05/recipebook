455368a8f36a6108d62b6290379ba8e8cdb44b62
Eric Hicks <ehicks05@yahoo.com>
2020-10-12 10:23:46 -0400

    Add missing annotation and add heroku-gradle plugin

--- src/main/java/net/hicks/recipe/Application.java
+++ src/main/java/net/hicks/recipe/Application.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -21,6 +22,7 @@ public class Application
         log.info("Recipe Book is starting.");
     }
 
+		@Bean
     public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
 			@Override
