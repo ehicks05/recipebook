d11694fbfefb4a3934246c393d50b8a60cdc5c76
Eric Hicks <ehicks05@yahoo.com>
2020-10-18 16:51:24 -0400

    Extract apiUrl to .env

--- src/react/apiUrl.ts
+++ src/react/apiUrl.ts
@@ -1,4 +1,6 @@
-export const apiUrl =
-  process.env.NODE_ENV === "development"
-    ? ""
-    : "https://hicks-recipe-book.herokuapp.com";
+const apiUrl =
+  process.env.NODE_ENV === "production"
+    ? (process.env.REACT_APP_API_URL as string)
+    : "";
+
+export default apiUrl;
