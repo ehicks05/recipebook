113032324518fba5bd9bb758cb21cc29af8785ab
Eric Hicks <ehicks05@yahoo.com>
2020-11-11 13:38:13 -0500

    Disable cors part 2

--- src/main/java/net/hicks/recipe/config/MvcConfig.java
+++ src/main/java/net/hicks/recipe/config/MvcConfig.java
@@ -3,7 +3,6 @@ package net.hicks.recipe.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.format.datetime.standard.DateTimeFormatterRegistrar;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -18,14 +17,4 @@ public class MvcConfig implements WebMvcConfigurer
         registrar.setUseIsoFormat(true);
         registrar.registerFormatters(registry);
     }
-
-   @Override
-   public void addCorsMappings(CorsRegistry registry) {
-       registry
-               .addMapping("/**")
-               .allowedOrigins("*")
-               .allowedMethods("*")
-               .allowedHeaders("*")
-               .allowCredentials(true);
-   }
 }
