2e5e9e79172155dd813691c6fe6f85dd46dd2fcf
shicks <shicks255@yahoo.com>
2020-11-15 21:32:20 -0600

    removing spring session for now

--- src/main/java/net/hicks/recipe/beans/HttpSessionInitializer.java
+++
@@ -1,6 +0,0 @@
-package net.hicks.recipe.beans;
-
-import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
-
-public class HttpSessionInitializer extends AbstractHttpSessionApplicationInitializer {
-}
