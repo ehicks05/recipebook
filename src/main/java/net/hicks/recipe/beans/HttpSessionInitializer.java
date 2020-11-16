fe27d5751e7fbb8736ea227f00aed776d7f28a61
shicks <shicks255@yahoo.com>
2020-11-15 20:53:47 -0600

    still more

---
+++ src/main/java/net/hicks/recipe/beans/HttpSessionInitializer.java
@@ -0,0 +1,6 @@
+package net.hicks.recipe.beans;
+
+import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
+
+public class HttpSessionInitializer extends AbstractHttpSessionApplicationInitializer {
+}
