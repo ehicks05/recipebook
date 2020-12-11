8b7cd3510aceb6942e78ce200b43048df2506200
shicks <shicks255@yahoo.com>
2020-12-10 21:12:45 -0600

    adding tests back

---
+++ src/react/components/FavoriteButton.tsx
@@ -0,0 +1,57 @@
+import {useState} from 'react';
+import {BiHeart, BsFillHeartFill} from "react-icons/all";
+import React from "react";
+import {IconContext} from "react-icons";
+import authFetch from "../authFetch";
+
+interface IProps {
+    recipeId: number | undefined;
+    favoriteIds: number[];
+    fetchFavorites: () => void;
+}
+
+function FavoriteButton({ recipeId, favoriteIds, fetchFavorites }: IProps) {
+    const [size, setSize] = useState<string>("1em");
+
+    function iconHover(e: React.MouseEvent) {
+        setSize("1.3em");
+    }
+
+    function iconLeaveHover(e: React.MouseEvent) {
+        setSize("1em");
+    }
+
+    function saveFavorite() {
+        authFetch(`/recipe/favorite/${recipeId}`, {
+            method: "POST"
+        }).then(json => {
+            console.log(json);
+            console.log('saved favorite');
+            fetchFavorites();
+        });
+
+    }
+
+    function removeFavorite() {
+        authFetch(`/recipe/favorite/${recipeId}`, {
+            method: "DELETE"
+        }).then(json => {
+            fetchFavorites();
+        });
+    }
+
+    return (
+        <div onClick={(e) => {}}>
+            <IconContext.Provider value={{size: size}}>
+                {
+                    (recipeId && favoriteIds.includes(recipeId))
+
+                        ? <BsFillHeartFill onMouseEnter={iconHover} onMouseLeave={iconLeaveHover} onClick={removeFavorite}  />
+                        : <BiHeart onMouseEnter={iconHover} onMouseLeave={iconLeaveHover} onClick={saveFavorite} />
+                }
+            </IconContext.Provider>
+        </div>
+    );
+}
+
+export default FavoriteButton;
