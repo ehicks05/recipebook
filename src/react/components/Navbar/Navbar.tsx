6d8a37802a7d91063a04663b2252368e0850be95
Eric Hicks <ehicks05@yahoo.com>
2020-12-16 23:00:59 -0500

    Clean up navbar

--- src/react/components/Navbar/Navbar.tsx
+++ src/react/components/Navbar/Navbar.tsx
@@ -1,39 +1,15 @@
-import React, { Dispatch, SetStateAction, useEffect } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { FaPlus } from 'react-icons/all';
-import { IUser } from '../../types/types';
 import UserAccess from './components/UserAccess';
 import { UserContext } from '../UserContext';
 
+const clickBurger = (e: React.MouseEvent<HTMLDivElement>) => {
+  (e.target as HTMLDivElement).classList.toggle('is-active');
+  document.getElementById('navbarBasicExample')?.classList.toggle('is-active');
+};
+
 function Navbar() {
-  useEffect(() => {
-    wireUpHamburgers();
-  }, []);
-
-  function wireUpHamburgers() {
-    // Get all "navbar-burger" elements
-    const $navbarBurgers = Array.prototype.slice.call(
-      document.querySelectorAll('.navbar-burger'),
-      0,
-    );
-
-    // Check if there are any navbar burgers
-    if ($navbarBurgers.length > 0) {
-      // Add a click event on each of them
-      $navbarBurgers.forEach(el => {
-        el.addEventListener('click', () => {
-          // Get the target from the "data-target" attribute
-          const { target } = el.dataset;
-          const $target = document.getElementById(target);
-
-          // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-          el.classList.toggle('is-active');
-          if ($target) $target.classList.toggle('is-active');
-        });
-      });
-    }
-  }
-
   return (
     <nav className="navbar" role="navigation" aria-label="main navigation">
       <div className="container">
@@ -57,10 +33,11 @@ function Navbar() {
 
           <div
             role="button"
+            tabIndex={0}
             className="navbar-burger burger"
             aria-label="menu"
             aria-expanded="false"
-            data-target="navbarBasicExample"
+            onClick={clickBurger}
           >
             <span aria-hidden="true" />
             <span aria-hidden="true" />
