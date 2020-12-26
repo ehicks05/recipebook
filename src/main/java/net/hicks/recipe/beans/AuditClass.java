4d94bec0d8b55eb4c09dff1841b582e8f745035f
Eric Hicks <ehicks05@yahoo.com>
2020-12-26 16:42:01 -0500

    Update field names

--- src/main/java/net/hicks/recipe/beans/AuditClass.java
+++ src/main/java/net/hicks/recipe/beans/AuditClass.java
@@ -3,34 +3,31 @@ package net.hicks.recipe.beans;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
-import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
 @MappedSuperclass
 public class AuditClass {
 
-    @Column(name = "added_on")
     @CreatedDate
-    private LocalDateTime addedOn;
+    private LocalDateTime createdAt;
 
-    @Column(name = "last_updated")
     @LastModifiedDate
-    private LocalDateTime lastUpdated;
+    private LocalDateTime updatedAt;
 
-    public LocalDateTime getAddedOn() {
-        return addedOn;
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
     }
 
-    public void setAddedOn(LocalDateTime addedOn) {
-        this.addedOn = addedOn;
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
     }
 
-    public LocalDateTime getLastUpdated() {
-        return lastUpdated;
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
     }
 
-    public void setLastUpdated(LocalDateTime lastUpdated) {
-        this.lastUpdated = lastUpdated;
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
     }
 }
