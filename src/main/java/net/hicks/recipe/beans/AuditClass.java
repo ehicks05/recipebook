1ebbbc8860d12808e03b968cd88b05c9a5287caa
Eric Hicks <ehicks05@yahoo.com>
2020-12-26 15:46:29 -0500

    Send audit fields in default format

--- src/main/java/net/hicks/recipe/beans/AuditClass.java
+++ src/main/java/net/hicks/recipe/beans/AuditClass.java
@@ -1,24 +1,20 @@
 package net.hicks.recipe.beans;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
 @MappedSuperclass
 public class AuditClass {
 
     @Column(name = "added_on")
-    @JsonFormat(pattern = "MMMM dd, yyyy")
     @CreatedDate
     private LocalDateTime addedOn;
 
     @Column(name = "last_updated")
-    @JsonFormat(pattern = "MMMM dd, yyyy")
     @LastModifiedDate
     private LocalDateTime lastUpdated;
 
