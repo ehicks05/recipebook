e45d6a4610fad807a79085c9ba92c157c0d15e0e
Eric Hicks <ehicks05@yahoo.com>
2021-08-28 02:15:18 -0400

    WIP

--- src/react/app/Recipe/Components/Direction.tsx
+++ src/react/app/Recipe/Components/Direction.tsx
@@ -1,13 +1,17 @@
-import React, { useState } from 'react';
+import React, { Dispatch, SetStateAction, useState } from 'react';
+import Highlighter, { HighlighterProps } from 'react-highlight-words';
 import Timer from './Timer';
-import { IDirection } from '../../../types/types';
+import { IDirection, IIngredient } from '../../../types/types';
 
 interface IDirectionProps {
+  ingredients: IIngredient[];
   direction: IDirection;
+  highlightedIngredient?: string;
+  setHighlightedIngredient: Dispatch<SetStateAction<string | undefined>>;
 }
 
 function extractTiming(text: string) {
-  const words = text.split(' ');
+  const words = text.split(/\W+/);
   const timeIndex = words.findIndex(
     word => word.indexOf('minute') > -1 || word.indexOf('hour') > -1,
   );
@@ -25,14 +29,19 @@ function extractTiming(text: string) {
   return timeAmount;
 }
 
-function Direction({ direction }: IDirectionProps) {
+function Direction({
+  ingredients,
+  direction,
+  highlightedIngredient,
+  setHighlightedIngredient,
+}: IDirectionProps) {
   const [isDone, setIsDone] = useState(false);
   const timeAmount = extractTiming(direction.text);
   const timer = timeAmount > 0 ? <Timer minutes={timeAmount} /> : null;
 
   return (
     <li
-      key={direction.text}
+      key={direction.index}
       style={{
         opacity: isDone ? '0.5' : '',
       }}
@@ -45,11 +54,118 @@ function Direction({ direction }: IDirectionProps) {
         }}
         onClick={() => setIsDone(!isDone)}
       >
-        {direction.text}
+        <ParsedText
+          ingredients={ingredients}
+          text={direction.text}
+          highlightedIngredient={highlightedIngredient}
+          setHighlightedIngredient={setHighlightedIngredient}
+        />
+        <Highlighter
+          highlightClassName="YourHighlightClass"
+          searchWords={ingredients.map(i => i.name)}
+          autoEscape
+          textToHighlight={direction.text}
+          highlightTag={HighlightTag}
+        />
       </div>
       {timer}
     </li>
   );
 }
+
+const HighlightTag = ({ children, highlightIndex }: HighlighterProps) => (
+  <mark>{children}</mark>
+);
+
+interface IParsedTextProps {
+  ingredients: IIngredient[];
+  text: string;
+  highlightedIngredient?: string;
+  setHighlightedIngredient: Dispatch<SetStateAction<string | undefined>>;
+}
+
+const ParsedText = ({
+  ingredients,
+  text,
+  highlightedIngredient,
+  setHighlightedIngredient,
+}: IParsedTextProps) => {
+  const words = text.split(/\W+/);
+
+  const matches = ingredients
+    .map(i => i.name)
+    .map(ingredient => {
+      const ingredientWords = ingredient.split(/\W+/);
+      const window = ingredientWords.length;
+
+      const ingredientMatches = words
+        .map((startWord, i) => {
+          const chunk = words.slice(i, i + window);
+          const match = chunk.join(' ') === ingredient;
+          return match ? [i, i + window - 1] : undefined;
+        })
+        .filter(i => i);
+      return { [ingredient]: ingredientMatches };
+    })
+    .filter(i => Object.values(i)[0].length > 0);
+
+  if (matches.length > 0) console.log(matches);
+
+  const matchedIndices = matches
+    .map(match => Object.values(match))
+    .flat()
+    .flat()
+    .filter(m => m)
+    .sort((o1, o2) => o1![0] - o2![0]);
+
+  console.log(matchedIndices);
+
+  const annotatedWords = words.map((word, i) => {
+    const matchInfo = matchedIndices.reduce(
+      (agg: { isMatch: boolean; ingredient: IIngredient | undefined }, curr) => {
+        const start = curr![0];
+        const end = curr![1];
+
+        if (i >= start && i <= end) {
+          const ingredientName = words.slice(start, end + 1).join(' ');
+          const ingredient = ingredients?.find(i => i.name === ingredientName);
+          return { isMatch: true, ingredient };
+        }
+        return agg;
+      },
+      { isMatch: false, ingredient: undefined },
+    );
+
+    const annotatedWord = matchInfo.isMatch ? (
+      <span
+        style={{
+          textDecoration: 'underline',
+          textDecorationStyle: 'dashed',
+          textUnderlineOffset: '3px',
+          fontWeight:
+            highlightedIngredient === matchInfo?.ingredient?.name
+              ? 'bold'
+              : 'normal',
+        }}
+        onMouseEnter={() => setHighlightedIngredient(matchInfo?.ingredient?.name)}
+        onMouseLeave={() => setHighlightedIngredient(undefined)}
+      >
+        {word}
+        {/* ({matchInfo?.ingredient?.quantity} {matchInfo?.ingredient?.unit}) */}
+      </span>
+    ) : (
+      <span>{word}</span>
+    );
+
+    return (
+      <>
+        {annotatedWord}
+        {i < words.length - 1 ? '  ' : undefined}
+      </>
+    );
+  });
+
+  return <>{annotatedWords}</>;
+};
 
 export default Direction;
--- src/react/app/Recipe/Components/Directions.tsx
+++ src/react/app/Recipe/Components/Directions.tsx
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { Dispatch, SetStateAction } from 'react';
 import { Direction } from '.';
-import { IDirection } from '../../../types/types';
+import { IDirection, IIngredient } from '../../../types/types';
 
 interface IProps {
+  ingredients: IIngredient[];
   directions: IDirection[];
+  highlightedIngredient?: string;
+  setHighlightedIngredient: Dispatch<SetStateAction<string | undefined>>;
 }
 
-function Directions({ directions }: IProps) {
+function Directions({
+  ingredients,
+  directions,
+  highlightedIngredient,
+  setHighlightedIngredient,
+}: IProps) {
   return (
     <div className="content">
       <ol style={{ marginLeft: '16px' }}>
         {directions.map(direction => (
-          <Direction key={direction.text} direction={direction} />
+          <Direction
+            key={direction.index}
+            ingredients={ingredients}
+            direction={direction}
+            highlightedIngredient={highlightedIngredient}
+            setHighlightedIngredient={setHighlightedIngredient}
+          />
         ))}
       </ol>
     </div>
