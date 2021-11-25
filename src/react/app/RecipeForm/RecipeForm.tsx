80452abca56dc8f6e687085e7797f0ef9f69e58d
Eric Hicks <ehicks05@yahoo.com>
2021-11-25 15:23:42 -0500

    recipe form

--- src/react/app/RecipeForm/RecipeForm.tsx
+++ src/react/app/RecipeForm/RecipeForm.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useHistory, useParams } from 'react-router-dom';
-import { FaPlus } from 'react-icons/all';
+import { FaPlus, MdDelete } from 'react-icons/all';
 
 import { FieldArray, Form, Formik } from 'formik';
 import Hero from '../../components/Hero';
@@ -60,221 +60,159 @@ function RecipeForm({ fetchRecipes, recipes }: IProps) {
           >
             {({ values, setFieldValue, isValid, isSubmitting }) => (
               <Form>
-                <div className="columns">
-                  <div className="column is-one-third">
-                    <div className="">
-                      <h2 className="subtitle is-4">Recipe Details</h2>
+                <div className="text-right">
+                  <button
+                    type="submit"
+                    disabled={!isValid || isSubmitting}
+                    className={`button is-success ${
+                      isSubmitting ? 'is-loading' : ''
+                    }`}
+                  >
+                    {`${recipe ? 'Save' : 'Create Recipe '}`}
+                  </button>
+                </div>
+                <div className="grid md:grid-cols-7 gap-4">
+                  <div className="col-span-2 flex flex-col gap-2">
+                    <h2 className="text-lg font-semibold">Recipe Details</h2>
 
-                      <MyInput label="Name" name="name" placeholder="Name" />
-                      <MyTextArea
-                        name="description"
-                        label="Description"
-                        placeholder="Description"
+                    <MyInput label="Name" name="name" placeholder="Name" />
+                    <MyTextArea
+                      name="description"
+                      label="Description"
+                      placeholder="Description"
+                    />
+
+                    <div className="flex">
+                      <MyInput
+                        type="number"
+                        name="cookingTime"
+                        label="Time"
+                        placeholder="Minutes"
+                        min="1"
                       />
-
-                      <div className="columns is-mobile is-variable is-1 is-multiline">
-                        <div className="column is-half">
-                          <MyInput
-                            type="number"
-                            name="cookingTime"
-                            label="Time"
-                            placeholder="Minutes"
-                            min="1"
-                          />
-                        </div>
-                        <div className="column is-half">
-                          <MyInput
-                            type="number"
-                            name="servings"
-                            label="Servings"
-                            placeholder="Servings"
-                            min="1"
-                          />
-                        </div>
-                        <div className="column">
-                          <div className="field">
-                            <label className="label">Emoji</label>
-                            <div className="control">
-                              <EmojiSelector
-                                data={{ value: values.emoji }}
-                                updateEmoji={code =>
-                                  setFieldValue('emoji', code)
-                                }
-                              />
-                            </div>
-                          </div>
-                        </div>
-                        <div className="column is-narrow">
-                          <MySelect label="Difficulty" name="difficulty">
-                            <option value="1">1</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
-                            <option value="4">4</option>
-                            <option value="5">5</option>
-                          </MySelect>
-                        </div>
+                      <MyInput
+                        type="number"
+                        name="servings"
+                        label="Serves"
+                        placeholder="Servings"
+                        min="1"
+                      />
+                      <MySelect label="Difficulty" name="difficulty">
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                      </MySelect>
+                    </div>
+                    <div>
+                      <label className="label">Emoji</label>
+                      <div className="control">
+                        <EmojiSelector
+                          data={{ value: values.emoji }}
+                          updateEmoji={code => setFieldValue('emoji', code)}
+                        />
                       </div>
                     </div>
                   </div>
-                  <div className="column">
-                    <div className="">
-                      <h2 className="subtitle is-4">Ingredients</h2>
-                      <FieldArray name="ingredients">
-                        {({ remove, push }) => (
+                  <div className="md:col-span-3">
+                    <h2 className="text-lg font-semibold">Ingredients</h2>
+                    <FieldArray name="ingredients">
+                      {({ remove, push }) => (
+                        <div className="flex flex-col gap-2">
+                          {values.ingredients.map((ingredient, index) => (
+                            <div key={ingredient.name} className="flex">
+                              <MyInput
+                                name={`ingredients.${index}.quantity`}
+                                placeholder="Quantity"
+                                isExpanded
+                              />
+                              <MySelect name={`ingredients.${index}.unit`}>
+                                {UNIT_OPTIONS}
+                              </MySelect>
+                              <MyInput
+                                name={`ingredients.${index}.name`}
+                                placeholder="Name"
+                              />
+                              <button
+                                type="button"
+                                className="button"
+                                onClick={e => {
+                                  e.preventDefault();
+                                  remove(index);
+                                }}
+                              >
+                                <MdDelete />
+                              </button>
+                            </div>
+                          ))}
                           <div>
-                            {values.ingredients.length > 0 &&
-                              values.ingredients.map((ingredient, index) => (
-                                <>
-                                  {index !== 0 && <hr />}
-                                  <div
-                                    key={ingredient.name}
-                                    className="columns is-mobile is-variable is-1"
-                                  >
-                                    <div className="column is-narrow">
-                                      <div className="control">
-                                        <input
-                                          type="text"
-                                          className="input is-static"
-                                          readOnly
-                                          value={`${index + 1}.`}
-                                          style={{
-                                            textAlign: 'right',
-                                            width: '1.35rem',
-                                          }}
-                                        />
-                                      </div>
-                                    </div>
-                                    <div className="column">
-                                      <MyInput
-                                        name={`ingredients.${index}.name`}
-                                        placeholder="Name"
-                                      />
-                                      <div className="columns is-mobile is-variable is-0">
-                                        <div className="column">
-                                          <MyInput
-                                            name={`ingredients.${index}.quantity`}
-                                            placeholder="Quantity"
-                                            isExpanded
-                                          />
-                                        </div>
-                                        <div className="column is-narrow">
-                                          <MySelect
-                                            name={`ingredients.${index}.unit`}
-                                          >
-                                            {UNIT_OPTIONS}
-                                          </MySelect>
-                                        </div>
-                                      </div>
-                                    </div>
-                                    <div className="column is-narrow">
-                                      <button
-                                        type="button"
-                                        className="delete has-background-danger"
-                                        onClick={e => {
-                                          e.preventDefault();
-                                          remove(index);
-                                        }}
-                                      />
-                                    </div>
-                                  </div>
-                                </>
-                              ))}
                             <button
                               type="button"
                               className="button is-success"
                               onClick={() => push(DEFAULT_INGREDIENT)}
                             >
-                              <span className="icon">
-                                <FaPlus />
-                              </span>
+                              <FaPlus />
                             </button>
                           </div>
-                        )}
-                      </FieldArray>
-                    </div>
+                        </div>
+                      )}
+                    </FieldArray>
                   </div>
-                  <div className="column">
+                  <div className="md:col-span-2">
                     <div className="">
-                      <h2 className="subtitle is-4">Directions</h2>
+                      <h2 className="text-lg font-semibold">Directions</h2>
                       <FieldArray name="directions">
                         {({ remove, push }) => (
-                          <div>
-                            {values.directions.length > 0 &&
-                              values.directions.map((direction, index) => (
-                                <div
-                                  key={direction.text}
-                                  className="columns is-mobile is-variable is-1"
-                                >
-                                  <div className="column is-narrow">
-                                    <div className="control">
-                                      <input
-                                        type="text"
-                                        className="input is-static"
-                                        readOnly
-                                        value={`${index + 1}.`}
-                                        style={{
-                                          textAlign: 'right',
-                                          width: '1.35rem',
-                                        }}
-                                      />
-                                    </div>
-                                    <MyHiddenInput
-                                      name={`directions.${index}.index`}
-                                    />
-                                  </div>
-                                  <div className="column">
-                                    <MyTextArea
-                                      name={`directions.${index}.text`}
-                                      placeholder="Description"
-                                    />
-                                  </div>
-                                  <div className="column is-narrow">
-                                    <button
-                                      type="button"
-                                      className="delete has-background-danger"
-                                      onClick={e => {
-                                        e.preventDefault();
-                                        remove(index);
-                                      }}
-                                    />
-                                  </div>
+                          <div className="flex flex-col gap-2">
+                            {values.directions.map((direction, index) => (
+                              <div key={direction.text} className="flex gap-2">
+                                <div className="">
+                                  <div className="control">{index + 1}.</div>
+                                  <MyHiddenInput
+                                    name={`directions.${index}.index`}
+                                  />
                                 </div>
-                              ))}
-                            <button
-                              type="button"
-                              className="button is-success"
-                              onClick={() =>
-                                push({
-                                  ...DEFAULT_DIRECTION,
-                                  index: values.directions.length,
-                                })
-                              }
-                            >
-                              <span className="icon">
+                                <div className="">
+                                  <MyTextArea
+                                    name={`directions.${index}.text`}
+                                    placeholder="Description"
+                                  />
+                                </div>
+                                <div className="">
+                                  <button
+                                    type="button"
+                                    className="button"
+                                    onClick={e => {
+                                      e.preventDefault();
+                                      remove(index);
+                                    }}
+                                  >
+                                    <MdDelete />
+                                  </button>
+                                </div>
+                              </div>
+                            ))}
+                            <div>
+                              <button
+                                type="button"
+                                className="button is-success"
+                                onClick={() =>
+                                  push({
+                                    ...DEFAULT_DIRECTION,
+                                    index: values.directions.length,
+                                  })
+                                }
+                              >
                                 <FaPlus />
-                              </span>
-                            </button>
+                              </button>
+                            </div>
                           </div>
                         )}
                       </FieldArray>
                     </div>
                   </div>
                 </div>
-                <nav className="level">
-                  <div className="level-item has-text-centered">
-                    <div>
-                      <button
-                        type="submit"
-                        disabled={!isValid || isSubmitting}
-                        className={`button is-success ${
-                          isSubmitting ? 'is-loading' : ''
-                        }`}
-                      >
-                        {`${recipe ? 'Save Changes' : 'Create Recipe '}`}
-                      </button>
-                    </div>
-                  </div>
-                </nav>
               </Form>
             )}
           </Formik>
