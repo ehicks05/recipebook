dc410b508d8d0b1e13a07ab8b16182b2aaf545fd
Eric Hicks <ehicks05@yahoo.com>
2021-11-29 21:33:28 -0500

    match width of rest of app

--- src/react/components/Navbar/Nav.tsx
+++ src/react/components/Navbar/Nav.tsx
@@ -75,7 +75,7 @@ export default function Example() {
     <Disclosure as="nav" className="bg-gray-800">
       {({ open }) => (
         <>
-          <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+          <div className="max-w-screen-xl mx-auto px-4">
             <div className="relative flex items-center justify-between h-16">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button */}
