3eef50becc963d479e4311e7c85f8804ba2d8346
Eric Hicks <ehicks05@yahoo.com>
2022-01-15 14:01:17 -0500

    hide create recipe nav link when logged out

--- src/react/components/Navbar/Nav.tsx
+++ src/react/components/Navbar/Nav.tsx
@@ -20,6 +20,7 @@ export default function Example() {
       name: 'Create Recipe',
       href: '/create-recipe',
       current: location.pathname.includes('/create-recipe'),
+      hidden: !user,
     },
     {
       name: 'Blog',
@@ -105,21 +106,23 @@ export default function Example() {
                 </div>
                 <div className="hidden sm:block sm:ml-6">
                   <div className="flex space-x-4">
-                    {navigation.map(item => (
-                      <Link
-                        key={item.name}
-                        to={item.href}
-                        className={classNames(
-                          item.current
-                            ? 'bg-neutral-900 text-white'
-                            : 'text-neutral-300 hover:bg-neutral-700 hover:text-white',
-                          'px-3 py-2 rounded-md text-sm font-medium'
-                        )}
-                        aria-current={item.current ? 'page' : undefined}
-                      >
-                        {item.name}
-                      </Link>
-                    ))}
+                    {navigation
+                      .filter(item => !item.hidden)
+                      .map(item => (
+                        <Link
+                          key={item.name}
+                          to={item.href}
+                          className={classNames(
+                            item.current
+                              ? 'bg-neutral-900 text-white'
+                              : 'text-neutral-300 hover:bg-neutral-700 hover:text-white',
+                            'px-3 py-2 rounded-md text-sm font-medium'
+                          )}
+                          aria-current={item.current ? 'page' : undefined}
+                        >
+                          {item.name}
+                        </Link>
+                      ))}
                   </div>
                 </div>
               </div>
@@ -153,21 +156,23 @@ export default function Example() {
 
           <Disclosure.Panel className="sm:hidden">
             <div className="px-2 pt-2 pb-3 space-y-1">
-              {navigation.map(item => (
-                <a
-                  key={item.name}
-                  onClick={() => navigate(item.href)}
-                  className={classNames(
-                    item.current
-                      ? 'bg-neutral-900 text-white'
-                      : 'text-neutral-300 hover:bg-neutral-700 hover:text-white',
-                    'block px-3 py-2 rounded-md text-base font-medium'
-                  )}
-                  aria-current={item.current ? 'page' : undefined}
-                >
-                  {item.name}
-                </a>
-              ))}
+              {navigation
+                .filter(item => !item.hidden)
+                .map(item => (
+                  <a
+                    key={item.name}
+                    onClick={() => navigate(item.href)}
+                    className={classNames(
+                      item.current
+                        ? 'bg-neutral-900 text-white'
+                        : 'text-neutral-300 hover:bg-neutral-700 hover:text-white',
+                      'block px-3 py-2 rounded-md text-base font-medium'
+                    )}
+                    aria-current={item.current ? 'page' : undefined}
+                  >
+                    {item.name}
+                  </a>
+                ))}
             </div>
           </Disclosure.Panel>
         </>
