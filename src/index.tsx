12b6af76789de674d526bbed25dfdb6e26d080e2
Eric Hicks <ehicks05@yahoo.com>
2022-03-13 10:10:29 -0400

    banner

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,17 @@ import { supabase } from './supabase';
 import App from './App';
 import './index.css';
 
+const banner = String.raw`
+______          _             ______             _    
+| ___ \        (_)            | ___ \           | |   
+| |_/ /___  ___ _ _ __   ___  | |_/ / ___   ___ | | __
+|    // _ \/ __| | '_ \ / _ \ | ___ \/ _ \ / _ \| |/ /
+| |\ \  __/ (__| | |_) |  __/ | |_/ / (_) | (_) |   < 
+\_| \_\___|\___|_| .__/ \___| \____/ \___/ \___/|_|\_\
+                 | |                                  
+                 |_|                                  `;
+console.log(banner);
+
 ReactDOM.render(
   <Auth.UserContextProvider supabaseClient={supabase}>
     <Provider value={supabase}>
