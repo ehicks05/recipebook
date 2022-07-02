b949cc4ed191a2a4bb103c896616997dadc4af50
Eric Hicks <ehicks05@yahoo.com>
2022-07-02 14:36:38 -0400

    add airbnb-typescript

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
   extends: [
     'plugin:react/recommended',
     'airbnb',
+    'airbnb-typescript',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
   ],
