5634912b799649b32cae931e66bb51d46020cf98
Eric Hicks <ehicks05@yahoo.com>
2023-01-19 00:27:52 -0500

    lint

--- src/components/Home/FavoriteButton.tsx
+++ src/components/Home/FavoriteButton.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import { HiHeart, HiOutlineHeart } from 'react-icons/hi';
+import React from "react";
+import { HiHeart, HiOutlineHeart } from "react-icons/hi";
 
-import {
-  useAddFavorite,
-  useFetchFavorites,
-  useRemoveFavorite,
-} from 'hooks/favorites';
-import { Button } from 'core-components';
+// import {
+//   useAddFavorite,
+//   useFetchFavorites,
+//   useRemoveFavorite,
+// } from 'hooks/favorites';
+// import { Button } from 'components/core';
 
 interface IProps {
   recipeId: string;
@@ -14,22 +14,24 @@ interface IProps {
 }
 
 function FavoriteButton({ recipeId, className }: IProps) {
-  const userFavorites = useFetchFavorites();
-  const addFavorite = useAddFavorite(recipeId);
-  const removeFavorite = useRemoveFavorite(recipeId);
+  // const userFavorites = useFetchFavorites();
+  // const addFavorite = useAddFavorite(recipeId);
+  // const removeFavorite = useRemoveFavorite(recipeId);
 
-  const favoriteIds = userFavorites.data?.map(f => f.id) || [];
+  // const favoriteIds = userFavorites.data?.map(f => f.id) || [];
 
-  const Icon = favoriteIds.includes(recipeId) ? HiHeart : HiOutlineHeart;
-  const handler = favoriteIds.includes(recipeId)
-    ? removeFavorite.mutate
-    : addFavorite.mutate;
+  // const Icon = favoriteIds.includes(recipeId) ? HiHeart : HiOutlineHeart;
+  // const handler = favoriteIds.includes(recipeId)
+  //   ? removeFavorite.mutate
+  //   : addFavorite.mutate;
 
-  return (
-    <Button className={className} onClick={() => handler()}>
-      <Icon className="text-2xl text-red-500" />
-    </Button>
-  );
+  // return (
+  //   <Button className={className} onClick={() => handler()}>
+  //     <Icon className="text-2xl text-red-500" />
+  //   </Button>
+  // );
+
+  return <div>ok</div>;
 }
 
 export default FavoriteButton;
