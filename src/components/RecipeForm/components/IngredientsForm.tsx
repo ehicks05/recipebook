1004cd29b1fbc0db059f33413982f67519b5e93d
Eric Hicks <ehicks05@yahoo.com>
2023-01-25 20:30:41 -0500

    scrollbar style, space between ingredients

--- src/components/RecipeForm/components/IngredientsForm.tsx
+++ src/components/RecipeForm/components/IngredientsForm.tsx
@@ -14,45 +14,47 @@ const IngredientsForm = ({ ingredientsFieldArray, register }: Props) => {
   return (
     <div className="flex flex-col gap-2 md:col-span-1 lg:col-span-2">
       <T className="text-lg font-semibold">Ingredients</T>
-      {ingredientsFieldArray.fields.map((field, index) => (
-        <div key={index} className="flex flex-col gap-2">
-          <MyHiddenInput name={`ingredients.${index}.index`} />
-          <div className="flex items-start gap-2">
+      <div className="flex flex-col gap-6">
+        {ingredientsFieldArray.fields.map((field, index) => (
+          <div key={index} className="flex flex-col gap-2">
+            <MyHiddenInput name={`ingredients.${index}.index`} />
+            <div className="flex items-start gap-2">
+              <MyInput
+                name={`ingredients.${index}.quantity`}
+                placeholder="Quantity"
+                register={register}
+              />
+              <MySelect name={`ingredients.${index}.unit`} register={register}>
+                {UNIT_OPTIONS}
+              </MySelect>
+              <Button
+                onClick={(e) => {
+                  e.preventDefault();
+                  ingredientsFieldArray.remove(index);
+                }}
+              >
+                <HiMinus />
+              </Button>
+            </div>
             <MyInput
-              name={`ingredients.${index}.quantity`}
-              placeholder="Quantity"
+              name={`ingredients.${index}.name`}
+              placeholder="Name"
               register={register}
             />
-            <MySelect name={`ingredients.${index}.unit`} register={register}>
-              {UNIT_OPTIONS}
-            </MySelect>
-            <Button
-              onClick={(e) => {
-                e.preventDefault();
-                ingredientsFieldArray.remove(index);
-              }}
-            >
-              <HiMinus />
-            </Button>
           </div>
-          <MyInput
-            name={`ingredients.${index}.name`}
-            placeholder="Name"
-            register={register}
-          />
+        ))}
+        <div>
+          <Button
+            onClick={() =>
+              ingredientsFieldArray.append({
+                ...DEFAULT_INGREDIENT,
+                // index: values.ingredients.length,
+              })
+            }
+          >
+            <HiPlus />
+          </Button>
         </div>
-      ))}
-      <div>
-        <Button
-          onClick={() =>
-            ingredientsFieldArray.append({
-              ...DEFAULT_INGREDIENT,
-              // index: values.ingredients.length,
-            })
-          }
-        >
-          <HiPlus />
-        </Button>
       </div>
     </div>
   );
