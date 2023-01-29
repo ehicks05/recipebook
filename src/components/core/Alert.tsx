de314ecd53b47625a87579b51f67664b886042ee
Eric Hicks <ehicks05@yahoo.com>
2023-01-29 13:41:15 -0500

    add success variant

--- src/components/core/Alert.tsx
+++ src/components/core/Alert.tsx
@@ -1,7 +1,24 @@
-import { HiXCircle } from "react-icons/hi";
+import { HiCheckCircle, HiXCircle } from "react-icons/hi";
+
+const VARIANTS = {
+  error: {
+    background: "bg-red-100",
+    title: "text-red-800",
+    description: "text-red-700",
+    icon: "text-red-600",
+    Icon: HiXCircle,
+  },
+  success: {
+    background: "bg-green-100",
+    title: "text-green-800",
+    description: "text-green-700",
+    icon: "text-green-600",
+    Icon: HiCheckCircle,
+  },
+} as const;
 
 interface Props {
-  variant: "error";
+  variant: keyof typeof VARIANTS;
   title: string;
   description?: string;
 }
@@ -11,14 +28,15 @@ const Alert = ({
   description,
   variant = "error",
 }: Props) => {
-  console.log(`todo: add more variants beyond '${variant}'`);
+  const classes = VARIANTS[variant];
+  const { Icon } = classes;
   return (
-    <div className="m-3 rounded bg-red-100 p-3">
+    <div className={`m-3 rounded p-3 ${classes.background}`}>
       <div className="flex items-center gap-2 text-lg font-semibold">
-        <HiXCircle className="text-red-600" />
-        <div className="text-red-800">{title}</div>
+        <Icon className={classes.icon} />
+        <div className={classes.title}>{title}</div>
       </div>
-      <div className="pl-7 text-red-700">{description}</div>
+      <div className={`pl-7 ${classes.description}`}>{description}</div>
     </div>
   );
 };
