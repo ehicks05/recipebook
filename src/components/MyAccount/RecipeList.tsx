de251a97b649837f43e42dbbc2bcf644abd0fbb8
Eric Hicks <ehicks05@yahoo.com>
2023-02-01 18:18:29 -0500

    minor cleanup

--- src/components/MyAccount/RecipeList.tsx
+++ src/components/MyAccount/RecipeList.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 import { T } from "components/core";
-import SmallRecipeCard from "./SmallRecipeCard";
 import type { CompleteRecipe } from "server/api/routers/example";
+// import { RecipeCardOld } from "components/Home/RecipeCard";
+import SmallRecipeCard from "./SmallRecipeCard";
 
-interface IMyAccountComponentType {
+interface Props {
   title: string;
   recipes: CompleteRecipe[] | undefined;
 }
 
-function MyAccountComponent({ title, recipes }: IMyAccountComponentType) {
+const RecipeList = ({ title, recipes }: Props) => {
   return (
-    <div>
-      <T className="mb-4 text-center text-lg font-semibold">{title}</T>
+    <div className="flex flex-col gap-4">
+      <T className="text-center text-lg font-semibold">{title}</T>
       <div className="flex flex-col gap-4">
         {recipes?.map((recipe) => (
           <SmallRecipeCard key={recipe.id} recipe={recipe} />
@@ -19,6 +20,6 @@ function MyAccountComponent({ title, recipes }: IMyAccountComponentType) {
       </div>
     </div>
   );
-}
+};
 
-export default MyAccountComponent;
+export default RecipeList;
--- src/pages/my-account/index.tsx
+++ src/pages/my-account/index.tsx
@@ -1,36 +1,40 @@
 import React from "react";
-import { Container, Hero, T } from "components/core";
-import MyAccountComponent from "components/MyAccount/MyAccountComponent";
+import { Container, Hero, MyInput, T } from "components/core";
+import RecipeList from "components/MyAccount/RecipeList";
 import { useUser } from "@supabase/auth-helpers-react";
 import { api } from "utils/api";
 
-function MyAccount() {
+const MyAccount = () => {
   const user = useUser();
+  if (!user) {
+    return null;
+  }
+  const id = user.id;
 
   const authoredRecipes = api.example.findRecipesByAuthorId.useQuery({
-    id: user?.id || "",
+    id,
   }).data;
   const userFavorites = api.example.findFavoriteRecipesByUserId.useQuery({
-    id: user?.id || "",
-  });
-  const favoriteRecipes = userFavorites.data?.map((o) => o.recipe);
+    id,
+  }).data;
+  const favoriteRecipes = userFavorites?.map((o) => o.recipe);
 
   return (
     <>
       <Hero title="Your Profile" subtitle={user?.email} />
       <Container>
-        <T>User Info Dump: </T>
-        <T className="whitespace-pre">{JSON.stringify(user, null, 2)}</T>
-      </Container>
-      <Container>
-        <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
-          <MyAccountComponent recipes={authoredRecipes} title="My Recipes" />
-          <MyAccountComponent recipes={favoriteRecipes} title="My Favorites" />
-          <MyAccountComponent recipes={[]} title="My Lists" />
+        <div className="grid grid-cols-1 gap-6 md:grid-cols-3">
+          <div className="flex flex-col gap-4">
+            <T className="text-xl">User Details</T>
+            <MyInput disabled label="email" value={user.email} />
+            {/* <T className="whitespace-pre">{JSON.stringify(user, null, 2)}</T> */}
+          </div>
+          <RecipeList recipes={authoredRecipes} title="My Recipes" />
+          <RecipeList recipes={favoriteRecipes} title="My Favorites" />
         </div>
       </Container>
     </>
   );
-}
+};
 
 export default MyAccount;
