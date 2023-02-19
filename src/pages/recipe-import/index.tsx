6cc55e03a6183c6b1de923975ea9f015c6090bd5
Eric Hicks <ehicks05@yahoo.com>
2023-02-19 11:55:54 -0500

    more warnings

--- src/pages/recipe-import/index.tsx
+++ src/pages/recipe-import/index.tsx
@@ -39,6 +39,11 @@ const RecipeImportPage: NextPage = () => {
           <div className="max-w-prose">
             Here you can paste the url of a recipe from the web. If it includes
             web-friendly metadata, you will be able to see it here.
+            <br />
+            <br />
+            Check the imported recipe carefully! Depending on how the website
+            formats their metadata, imported recipes can wind up with parts
+            missing or garbled.
           </div>
         </Alert>
       )}
