01be154250f8d7a38707e0cda6831c47b22828c6
Eric Hicks <ehicks05@yahoo.com>
2023-04-09 12:42:45 -0400

    put import alert in container

--- src/pages/recipe-import/index.tsx
+++ src/pages/recipe-import/index.tsx
@@ -35,17 +35,19 @@ const RecipeImportPage: NextPage = () => {
   return (
     <>
       {!url && (
-        <Alert variant="info" title="What is this page?">
-          <div className="max-w-prose">
-            Here you can paste the url of a recipe from the web. If it includes
-            web-friendly metadata, you will be able to see it here.
-            <br />
-            <br />
-            Check the imported recipe carefully! Depending on how the website
-            formats their metadata, imported recipes can wind up with parts
-            missing or garbled.
-          </div>
-        </Alert>
+        <Container>
+          <Alert variant="info" title="What is this page?">
+            <div className="max-w-prose">
+              Here you can paste the url of a recipe from the web. If it
+              includes web-friendly metadata, you will be able to see it here.
+              <br />
+              <br />
+              Check the imported recipe carefully! Depending on how the website
+              formats their metadata, imported recipes can wind up with parts
+              missing or garbled.
+            </div>
+          </Alert>
+        </Container>
       )}
       <Container>
         <div className="flex items-start">
