9b8ad1404a12d330b067485afb7e658b4182478b
Eric Hicks <ehicks05@yahoo.com>
2023-11-24 17:37:15 -0500

    remove old recipe card

--- src/components/Home/RecipeCard.tsx
+++ src/components/Home/RecipeCard.tsx
@@ -1,23 +1,11 @@
 import React from "react";
 import Link from "next/link";
-import { Card, CookingTime, Difficulty, RecipeImage, T } from "components/core";
+import { Card, RecipeImage, T } from "components/core";
 import type { CompleteRecipe } from "server/api/routers/example";
 import FavoriteButton from "components/Home/FavoriteButton";
 import { useUser } from "@supabase/auth-helpers-react";
 import { HiOutlineClock } from "react-icons/hi";
 import { DIFFICULTIES } from "components/core/Difficulty";
-
-interface FooterProps {
-  cookingTime: string;
-  difficulty: number;
-}
-
-const Footer = ({ cookingTime, difficulty }: FooterProps) => (
-  <footer className="flex w-fit gap-4 pt-4">
-    <CookingTime cookingTime={cookingTime} />
-    <Difficulty difficulty={difficulty} />
-  </footer>
-);
 
 interface Props {
   recipe: CompleteRecipe;
@@ -58,8 +46,6 @@ const RecipeCard = ({
         </div>
         <T className="line-clamp-2 text-sm">{description}</T>
       </div>
-
-      {/* <Footer cookingTime={cookingTime} difficulty={difficulty} /> */}
     </Card>
   );
 };
@@ -97,34 +83,5 @@ function RecipeCardLandscape({
   );
 }
 
-function RecipeCardOld({
-  recipe: { id, emoji, name, author, description, cookingTime, difficulty },
-}: Props) {
-  const user = useUser();
-
-  return (
-    <Card className="transform border border-neutral-100 transition-all hover:shadow-md dark:border-neutral-700">
-      <div className="flex h-56 flex-col gap-4">
-        {/* title row */}
-        <div className="flex items-start gap-2">
-          <figure className="h-16 w-16 pt-2 text-5xl">{emoji}</figure>
-          <div className="w-full">
-            <Link href={`/recipe/${id}`}>
-              <div className="font-semibold dark:text-neutral-200">{name}</div>
-              <div className="text-xs italic dark:text-neutral-200">
-                {author.displayName}
-              </div>
-            </Link>
-          </div>
-          {user && <FavoriteButton recipeId={id} />}
-        </div>
-        <T className="text-sm line-clamp-5">{description}</T>
-      </div>
-
-      <Footer cookingTime={cookingTime} difficulty={difficulty} />
-    </Card>
-  );
-}
-
 export default RecipeCard;
-export { RecipeCardLandscape, RecipeCardOld };
+export { RecipeCardLandscape };
