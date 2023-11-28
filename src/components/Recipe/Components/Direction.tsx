bab0c796e575688431c48b378690b7dc696e8d2c
Eric Hicks <ehicks05@yahoo.com>
2023-11-27 20:24:22 -0500

    times up: remove Timer

--- src/components/Recipe/Components/Direction.tsx
+++ src/components/Recipe/Components/Direction.tsx
@@ -1,38 +1,13 @@
 import React, { useState } from "react";
 import { T } from "components/core";
-import Timer from "./Timer";
 import type { direction } from "@prisma/client";
-
-const firstTimeUnitIndex = (input: string) =>
-  input.indexOf("second") > -1 ||
-  input.indexOf("minute") > -1 ||
-  input.indexOf("hour") > -1;
 
 interface Props {
   direction: direction;
 }
 
-function extractTiming(text = "") {
-  const words = text.split(" ");
-  const timeIndex = words.findIndex(firstTimeUnitIndex);
-  let timeAmount = 0;
-  if (timeIndex > -1) {
-    timeAmount = parseInt(words[timeIndex - 1] || "", 10);
-    if (Number.isNaN(timeAmount)) timeAmount = 0;
-
-    const isHours = (words[timeIndex] || "").indexOf("hour") > -1;
-    if (isHours) timeAmount *= 60;
-    const isSeconds = (words[timeIndex] || "").indexOf("second") > -1;
-    if (isSeconds) timeAmount /= 60;
-  }
-
-  return timeAmount;
-}
-
 function Direction({ direction }: Props) {
   const [isDone, setIsDone] = useState(false);
-  const timeAmount = extractTiming(direction.text);
-  const timer = timeAmount > 0 ? <Timer minutes={timeAmount} /> : null;
 
   return (
     <li
@@ -42,9 +17,6 @@ function Direction({ direction }: Props) {
       <div onClick={() => setIsDone(!isDone)}>
         <T className={isDone ? "line-clamp-1" : ""}>{direction.text}</T>
       </div>
-      {timeAmount > 0 && (
-        <div className={`mt-1 ${isDone ? "hidden" : ""}`}>{timer}</div>
-      )}
     </li>
   );
 }
