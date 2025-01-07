97fa40c4afe11736e9b82b9b9e57e25353890c5c
Eric Hicks <ehicks05@yahoo.com>
2025-01-07 16:59:36 -0500

    global

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,13 +1,19 @@
 import { PrismaClient } from '@prisma/client';
 import { env } from '../env/server.mjs';
 
-export const prisma = new PrismaClient({
-	log:
-		env.NODE_ENV === 'development'
-			? [
-					// "query",
-					'error',
-					'warn',
-				]
-			: ['error'],
-});
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+export const prisma =
+	globalForPrisma.prisma ||
+	new PrismaClient({
+		log:
+			env.NODE_ENV === 'development'
+				? [
+						// "query",
+						'error',
+						'warn',
+					]
+				: ['error'],
+	});
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
