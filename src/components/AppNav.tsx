29c808d74f6c89880fb442b20a20e6d5fdf592e2
Eric Hicks <ehicks05@yahoo.com>
2025-01-08 12:15:31 -0500

    nit

--- src/components/AppNav.tsx
+++ src/components/AppNav.tsx
@@ -7,7 +7,7 @@ import {
 	UserButton,
 	useUser,
 } from '@clerk/nextjs';
-import { Disclosure } from '@headlessui/react';
+import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/react';
 import clsx from 'clsx';
 import Link from 'next/link';
 import { usePathname, useRouter } from 'next/navigation';
@@ -51,14 +51,14 @@ const Nav = () => {
 						<div className="relative flex h-16 items-center justify-between">
 							<div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
 								{/* Mobile menu button */}
-								<Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-neutral-400 hover:bg-neutral-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+								<DisclosureButton className="inline-flex items-center justify-center rounded-md p-2 text-neutral-400 hover:bg-neutral-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
 									<span className="sr-only">Open main menu</span>
 									{open ? (
 										<HiX className="block h-6 w-6" aria-hidden="true" />
 									) : (
 										<HiMenu className="block h-6 w-6" aria-hidden="true" />
 									)}
-								</Disclosure.Button>
+								</DisclosureButton>
 							</div>
 							<div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
 								<SiteLogo />
@@ -97,7 +97,7 @@ const Nav = () => {
 						</div>
 					</div>
 
-					<Disclosure.Panel className="sm:hidden">
+					<DisclosurePanel className="sm:hidden">
 						<div className="space-y-1 px-2 pb-3 pt-2">
 							{navigation
 								.filter((item) => !item.hidden)
@@ -118,7 +118,7 @@ const Nav = () => {
 									</button>
 								))}
 						</div>
-					</Disclosure.Panel>
+					</DisclosurePanel>
 				</>
 			)}
 		</Disclosure>
