c79cd4995d0376503f341898b881a34ab12a0e59
Eric Hicks <ehicks05@yahoo.com>
2025-01-08 13:35:12 -0500

    split

--- src/pages/recipe-import/index.tsx
+++ src/pages/recipe-import/index.tsx
@@ -7,6 +7,21 @@ import { useState } from 'react';
 import { api } from 'utils/api';
 import { parseLdJsonRecipe } from 'utils/recipe-import';
 
+const Instructions = () => (
+	<Container>
+		<Alert variant="info" title="What is this page?">
+			<div className="max-w-prose">
+				Here you can paste the url of a recipe from the web. If it includes
+				web-friendly metadata, you will be able to see it here.
+				<br />
+				<br />
+				Check the imported recipe carefully! Depending on how the website formats
+				their metadata, imported recipes can wind up with parts missing or garbled.
+			</div>
+		</Alert>
+	</Container>
+);
+
 const RecipeImportPage: NextPage = () => {
 	const router = useRouter();
 	const { url } = router.query;
@@ -34,21 +49,7 @@ const RecipeImportPage: NextPage = () => {
 
 	return (
 		<>
-			{!url && (
-				<Container>
-					<Alert variant="info" title="What is this page?">
-						<div className="max-w-prose">
-							Here you can paste the url of a recipe from the web. If it includes
-							web-friendly metadata, you will be able to see it here.
-							<br />
-							<br />
-							Check the imported recipe carefully! Depending on how the website
-							formats their metadata, imported recipes can wind up with parts missing
-							or garbled.
-						</div>
-					</Alert>
-				</Container>
-			)}
+			{!url && <Instructions />}
 			<Container>
 				<div className="flex items-start">
 					<MyInput
