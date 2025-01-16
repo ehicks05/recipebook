017f9a209b2f7b649488680a05304134824058a1
Eric Hicks <ehicks05@yahoo.com>
2025-01-15 22:04:53 -0500

    remove supabase api client

--- src/env.ts
+++ src/env.ts
@@ -5,9 +5,6 @@ export const env = createEnv({
 	server: {
 		DATABASE_URL: z.string().url(),
 		NODE_ENV: z.enum(['development', 'test', 'production']),
-		SUPABASE_URL: z.string(),
-		SUPABASE_ANON_KEY: z.string(),
-		SUPABASE_ADMIN_KEY: z.string(),
 		CLERK_SECRET_KEY: z.string(),
 		WEBHOOK_SECRET: z.string(),
 		UPLOADTHING_TOKEN: z.string(),
@@ -16,13 +13,11 @@ export const env = createEnv({
 		OTEL_EXPORTER_OTLP_HEADERS: z.string(),
 	},
 	client: {
-		NEXT_PUBLIC_SUPABASE_PROJECT_ID: z.string(),
 		NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string(),
 		NEXT_PUBLIC_UPLOADTHING_APP_ID: z.string(),
 	},
 	// For Next.js >= 13.4.4, you only need to destructure client variables:
 	experimental__runtimeEnv: {
-		NEXT_PUBLIC_SUPABASE_PROJECT_ID: process.env.NEXT_PUBLIC_SUPABASE_PROJECT_ID,
 		NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
 		NEXT_PUBLIC_UPLOADTHING_APP_ID: process.env.NEXT_PUBLIC_UPLOADTHING_APP_ID,
 	},
--- src/utils/supabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-import { env } from 'env';
-
-const { SUPABASE_URL, SUPABASE_ADMIN_KEY } = env;
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ADMIN_KEY);
