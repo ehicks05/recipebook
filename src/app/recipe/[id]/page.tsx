cb63479d0bc4a176306976c76283186b11cf60ad
Eric Hicks <ehicks05@yahoo.com>
2025-01-16 23:20:52 -0500

    cleanup

--- src/app/recipe/[id]/page.tsx
+++ src/app/recipe/[id]/page.tsx
@@ -1,20 +1,12 @@
 import Recipe from 'components/Recipe/Recipe';
 import { notFound } from 'next/navigation';
-import { db } from 'server/db-api';
-
-export const revalidate = 86400; // 1 day
-export const dynamicParams = true;
-
-export async function generateStaticParams() {
-	const recipes = await db.recipes.recipes(null);
-	return recipes.map((recipe) => ({ id: recipe.id }));
-}
+import { api } from 'trpc/server';
 
 type Params = Promise<{ id: string }>;
 
 export default async function Page({ params }: { params: Params }) {
 	const { id } = await params;
-	const recipe = await db.recipes.recipeById(id);
+	const recipe = await api.example.findRecipe({ id });
 
 	if (!recipe) return notFound();
 
--- src/app/test/page.tsx
+++
@@ -1,9 +0,0 @@
-export const revalidate = 3600;
-
-export async function generateStaticParams() {
-	return [];
-}
-
-export default function Page() {
-	return <div>yo</div>;
-}
