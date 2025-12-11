b1b3760a46ef5385ed37b690e63b2dfd7d20a844
Eric Hicks <ehicks05@yahoo.com>
2025-12-11 14:09:55 -0500

    .

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -2,7 +2,7 @@ import { PrismaInstrumentation } from '@prisma/instrumentation';
 import { registerOTel } from '@vercel/otel';
 
 const prismaInstrumentation = new PrismaInstrumentation({
-	middleware: true, // Enable middleware tracing if needed
+	// middleware: true, // Enable middleware tracing if needed
 });
 
 export function register() {
