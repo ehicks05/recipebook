723062cdde08546d39143266759f4aed8ca719f8
Eric Hicks <ehicks05@yahoo.com>
2026-04-29 21:00:52 -0400

    clean

--- src/components/core/RecipeImage.tsx
+++ src/components/core/RecipeImage.tsx
@@ -1,44 +1,25 @@
-import { defaultImage, emojiToImage } from 'components/Home/constants';
 import { FaPizzaSlice } from 'react-icons/fa';
 import type { File } from '@/instant.types';
-
-// import { UPLOADTHING_BASE_URL } from "uploadthing/constants";
 
 interface Props {
 	image?: File | null;
 	emoji?: string;
-	emojiSize?: string;
-	height?: number;
-	width?: number;
 	className?: string;
-	onClick?: () => void;
 }
 
-export const RecipeImage = ({
-	image,
-	emoji,
-	emojiSize = 'text-8xl',
-	height = 300,
-	width = 400,
-	className,
-	onClick,
-}: Props) => {
+export const RecipeImage = ({ image, emoji, className }: Props) => {
 	return (
-		<div
-			className={`flex items-center justify-center ${className}`}
-			onClick={onClick}
-			onKeyUp={onClick}
-		>
+		<div className={`flex items-center justify-center ${className}`}>
 			{image && (
 				<img
 					className={`object-cover w-full ${className}`}
 					src={image.url}
 					alt="recipeImage"
-					height={height}
-					width={width}
+					height={300}
+					width={400}
 				/>
 			)}
-			{!image && emoji && <span className={emojiSize}>{emoji}</span>}
+			{!image && emoji && <span className="text-8xl">{emoji}</span>}
 			{!image && !emoji && <FaPizzaSlice className="w-3/4 h-3/4 opacity-25" />}
 		</div>
 	);
