ebc7c77b781d3953a670944c044b9524e85d9040
Eric Hicks <ehicks05@yahoo.com>
2026-05-01 13:04:15 -0400

    refactor

--- src/features/RecipeBrowser/FavoriteButton.tsx
+++ src/features/RecipeBrowser/FavoriteButton.tsx
@@ -3,7 +3,29 @@ import { Button } from '@/components/core';
 import { clientDb } from '@/lib/db';
 import { toast } from '@/lib/toast';
 import { cn } from '@/lib/utils';
-import { toggleFavorite } from './toggleFavorite';
+
+interface Params {
+	userId: string;
+	recipeId: string;
+	isFavorite: boolean;
+}
+
+export const toggleFavorite = async ({ userId, recipeId, isFavorite }: Params) => {
+	try {
+		clientDb.transact(
+			isFavorite
+				? clientDb.tx.$users[userId].unlink({ favoriteRecipes: recipeId })
+				: clientDb.tx.$users[userId].link({ favoriteRecipes: recipeId }),
+		);
+	} catch (e) {
+		const err = e instanceof Error ? e : undefined;
+		return {
+			err,
+			title: 'Unable to update favorites',
+		};
+	}
+	return { title: 'Favorites updated' };
+};
 
 interface Props {
 	recipeId: string;
@@ -13,16 +35,12 @@ interface Props {
 
 export const FavoriteButton = ({ recipeId, className, favoritedBy }: Props) => {
 	const { id: userId } = clientDb.useUser();
-	const isUserFavorite = favoritedBy.map((o) => o.id).includes(userId);
+	const isFavorite = favoritedBy.map((o) => o.id).includes(userId);
 
-	const Icon = isUserFavorite ? HiHeart : HiOutlineHeart;
+	const Icon = isFavorite ? HiHeart : HiOutlineHeart;
 
 	const handleClick = async () => {
-		const { title, err } = await toggleFavorite({
-			userId,
-			recipeId,
-			isUserFavorite,
-		});
+		const { title, err } = await toggleFavorite({ userId, recipeId, isFavorite });
 
 		toast({
 			variant: err ? 'error' : 'success',
