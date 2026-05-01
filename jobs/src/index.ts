ba5937c9c717bc61b17f93528a0222fa215f543e
Eric Hicks <ehicks05@yahoo.com>
2026-05-01 17:56:53 -0400

    clean

--- jobs/src/index.ts
+++
@@ -1,8 +0,0 @@
-import { Cron } from 'croner';
-import { featureRecipe } from './feature-recipe.js';
-
-const job = new Cron('0 0 * * *', () => {
-	featureRecipe();
-});
-
-console.log(`featureRecipe next running at ${job.nextRun()}`);
