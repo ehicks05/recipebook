cf6e5f2d412f05e635f5978ffd415e2a8ac3b6f0
Eric Hicks <ehicks05@yahoo.com>
2026-04-30 21:08:36 -0400

    style

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -2,6 +2,7 @@ import { createFileRoute, redirect, useNavigate } from '@tanstack/react-router';
 import { useServerFn } from '@tanstack/react-start';
 import { useEffect, useState } from 'react';
 import z from 'zod';
+import { Card } from '@/components/core';
 import { clientDb } from '@/lib/db';
 import { getImageUrl } from '@/middleware/getImageUrl';
 
@@ -67,93 +68,82 @@ function RouteComponent() {
 	};
 
 	return (
-		<div className="p-8 sm:grid-cols-2 items-start gap-2 grid-cols-1 grid">
-			<Welcome />
-			<div className="rounded-lg p-6 border border-neutral-200 shadow flex flex-col gap-4">
-				<h2 className="tracking-wide text-[#F54A00] text-2xl">
-					{stage === 'email' ? 'Sign In' : 'Enter Code'}
-				</h2>
-				<p className="text-xs">
-					{stage === 'email'
-						? 'Enter your email to receive a magic code'
-						: `We sent a code to ${emailInput}`}
-				</p>
-				<div className="rounded">
-					{stage === 'email' ? (
-						<>
-							<form
-								className="flex items-center border border-neutral-300 h-10"
-								onSubmit={(e) => {
-									e.preventDefault();
-									sendEmail();
-								}}
+		<Card className="flex flex-col gap-4 p-6 w-full max-w-prose mx-auto">
+			<h2 className="font-bold text-[#F54A00] text-2xl">
+				{stage === 'email' ? 'Sign In' : 'Enter Code'}
+			</h2>
+			<p className="text-sm">
+				{stage === 'email'
+					? 'Enter your email to receive a magic code'
+					: `We sent a code to ${emailInput}`}
+			</p>
+			<div className="rounded">
+				{stage === 'email' ? (
+					<>
+						<form
+							className="flex items-center border border-neutral-300 h-10"
+							onSubmit={(e) => {
+								e.preventDefault();
+								sendEmail();
+							}}
+						>
+							<input
+								type="email"
+								value={emailInput}
+								onChange={(e) => setEmailInput(e.target.value)}
+								placeholder="you@example.com"
+								required
+								autoFocus
+								className="flex-1 h-full px-2 outline-none bg-transparent"
+							/>
+							<button
+								type="submit"
+								disabled={!emailInput}
+								className="h-full cursor-pointer px-2 border-l border-0 border-neutral-300 bg-accent"
 							>
-								<input
-									type="email"
-									value={emailInput}
-									onChange={(e) => setEmailInput(e.target.value)}
-									placeholder="you@example.com"
-									required
-									autoFocus
-									className="flex-1 h-full px-2 outline-none bg-transparent"
-								/>
-								<button
-									type="submit"
-									disabled={!emailInput}
-									className="h-full cursor-pointer px-2 border-l border-0 border-neutral-300 bg-accent"
-								>
-									Send
-								</button>
-							</form>
-							<div className="h-7" />
-						</>
-					) : (
-						<>
-							<form
-								className="flex items-center h-10 border border-neutral-300"
-								onSubmit={(e) => {
-									e.preventDefault();
-									loginWithCode();
-								}}
+								Send
+							</button>
+						</form>
+						<div className="h-7" />
+					</>
+				) : (
+					<>
+						<form
+							className="flex items-center h-10 border border-neutral-300"
+							onSubmit={(e) => {
+								e.preventDefault();
+								loginWithCode();
+							}}
+						>
+							<input
+								type="text"
+								value={codeInput}
+								onChange={(e) => setCodeInput(e.target.value)}
+								placeholder="Enter your code"
+								required
+								autoFocus
+								className="flex-1 h-full px-2 outline-none bg-transparent"
+							/>
+							<button
+								type="submit"
+								disabled={!codeInput}
+								className="h-full px-2 cursor-pointer border-l border-neutral-300"
 							>
-								<input
-									type="text"
-									value={codeInput}
-									onChange={(e) => setCodeInput(e.target.value)}
-									placeholder="Enter your code"
-									required
-									autoFocus
-									className="flex-1 h-full px-2 outline-none bg-transparent"
-								/>
-								<button
-									type="submit"
-									disabled={!codeInput}
-									className="h-full px-2 cursor-pointer border-l border-neutral-300"
-								>
-									Submit
-								</button>
-							</form>
-							<div className="flex justify-center items-center h-10 px-2 text-xs">
-								<button
-									type="button"
-									onClick={goBack}
-									className="text-muted-foreground"
-								>
-									Use a different email
-								</button>
-							</div>
-						</>
-					)}
-				</div>
+								Submit
+							</button>
+						</form>
+						<div className="flex justify-center items-center h-10 px-2 text-xs">
+							<button
+								type="button"
+								onClick={goBack}
+								className="text-muted-foreground"
+							>
+								Use a different email
+							</button>
+						</div>
+					</>
+				)}
 			</div>
-		</div>
-	);
-}
-
-function Welcome() {
-	return (
-		<div className="border p-2 shadow flex py-8 flex-col gap-2 items-center justify-center font-semibold border-neutral-200 rounded">
-			Welcome to RecipeBook!
-		</div>
+		</Card>
 	);
 }
