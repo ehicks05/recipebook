35826f9ca6a0836dd5ff6e474c1b52c26141c65b
Eric Hicks <ehicks05@yahoo.com>
2026-05-01 14:59:08 -0400

    disable auto attr creation

--- src/instant.perms.ts
+++ src/instant.perms.ts
@@ -3,22 +3,11 @@
 import type { InstantRules } from '@instantdb/react';
 
 const rules = {
-	/**
-	 * Welcome to Instant's permission system!
-	 * Right now your rules are empty. To start filling them in, check out the docs:
-	 * https://www.instantdb.com/docs/permissions
-	 *
-	 * Here's an example to give you a feel:
-	 * posts: {
-	 *   allow: {
-	 *     view: "true",
-	 *     create: "isOwner",
-	 *     update: "isOwner",
-	 *     delete: "isOwner",
-	 *   },
-	 *   bind: {"isOwner": "auth.id != null && auth.id == data.ownerId"},
-	 * },
-	 */
+	attrs: {
+		allow: {
+			create: 'false',
+		},
+	},
 	$files: {
 		allow: {
 			view: 'true',
